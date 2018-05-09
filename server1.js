7dc33fdf26573ce1124a7d6e26bc2a9770b1b625
CharlineDelkhah <charlinedelkhah@yahoo.com>
2018-05-08 19:13:59 -0700

    knows when you’re passing in a query and it should also work for errors

--- server1.js
+++ server1.js
@@ -1,3 +1,4 @@
+
 var static = require('node-static');
 var http =  require('http');
  
@@ -6,14 +7,53 @@ var fileServer = new static.Server('./public');
 var server = http.createServer(handler);
 
 function handler (request, response) {
+    var url = request.url;
     request.addListener('end', function () {
-    	//check to see if there is query in the URL
-    	//if so call a new function
-
+    if(url.split("/")[1] == 'query'){
+        if (url.split("/")[2] <= '989'){
+            //here is where we should put the code if you get a query response.              
+        }
+    }
+    else if(url.split("/")[1] !='testWHS.html'){  //this will happen if they write something other than testWHS.html
+        response.writeHead(404, {"Content-type": "text/html"});
+        response.write("<p> 404 Not Found. </p>");
+        response.end();
+    }
     	//print out on console
-    	console.log(request.url); //url property of the rquest object (string)
-        fileServer.serve(request, response);
+    console.log(request.url); //url property of the rquest object (string)
+
+    fileServer.serve(request, response);
     }).resume();
 }
 
-server.listen(54022);
+
+
+
+/*var http =  require('http');
+var static = require('node-static');
+ 
+var fileServer = new static.Server('./public');
+
+var server = http.createServer(handler);
+
+function handler (request, response) {
+    var url = request.url;
+    
+    if(url.split("/")[1] !='testWHS.html'){  //this will happen if they write something other than testWHS.html
+        response.writeHead(404, {"Content-type": "text/html"});
+        response.write("<p> 404 Not Found. </p>");
+        response.end();
+    }
+    else{
+        request.addListener('end', searchBook).resume();
+    }
+
+    function searchBook(request, response){
+        fileServer.serve(request, response);
+    }
+
+}
+    	//print out on console
+    	//console.log(request.url); //url property of the rquest object (string)
+*/
+server.listen("58037");
